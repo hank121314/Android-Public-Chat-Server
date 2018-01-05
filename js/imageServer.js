427a9b5b8703daab6ad809a0c9db098adcd7f4f6
陳泓均 <hankchen@ekopro.solutions>
2018-01-05 10:23:11 +0800

    fix(js/imageServer):board request headers can be chinese

--- js/imageServer.js
+++ js/imageServer.js
@@ -1,9 +1,9 @@
 import { success } from 'Schema/Success';
 import { error } from 'Schema/Failed';
+import { Buffer } from 'buffer';
 
 const multer = require('multer');
 const fs = require('fs');
-
 const createFolder = folder => {
   try {
     fs.accessSync(folder);
@@ -15,12 +15,14 @@ const createFolder = folder => {
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     const str = file.originalname.split('-');
-    const uploadFolder = './data/image/boards/' + str[0] + '/';
+    const folder = decodeURIComponent(str[0]);
+    console.log(folder);
+    const uploadFolder = './data/image/boards/' + folder + '/';
     createFolder(uploadFolder);
-    cb(null, uploadFolder); // 保存的路径，备注：需要自己创建
+    cb(null, uploadFolder);
   },
   filename: function(req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, decodeURIComponent(file.originalname));
   },
 });
 
@@ -34,20 +36,23 @@ export const uploaderPhotoSticker = app => {
 
 export const downloadPhotoSticker = app => {
   app.get('/download/boardsPhoto', function(req, res) {
-    console.log('Send user', req.headers.boards);
+    const buffer = new Buffer.from(req.headers.boards);
+    const header = decodeURIComponent(req.headers.boards);
+    console.log('Request File', header);
     const touch = req.headers.touch;
     console.log(touch);
-    var folder = './data/image/boards/' + req.headers.boards + '/';
+    var folder = './data/image/boards/' + header + '/';
     var file;
     const allFile = [];
     fs.readdir(folder, (err, files) => {
       files.forEach(i => {
-        allFile.push(i);
+        const file = encodeURIComponent(i);
+        allFile.push(file);
       });
-      console.log('SendFile', allFile[allFile.length - 1]);
+      console.log('SendFile', decodeURIComponent(allFile[allFile.length - 1]));
       touch === 'true'
-        ? res.download(folder + allFile[allFile.length - 1])
-        : res.status(200).send(allFile[allFile.length - 1]);
+        ? res.download(folder + decodeURIComponent(allFile[allFile.length - 1]))
+        : res.status(200).send(decodeURIComponent(allFile[allFile.length - 1]));
       // Set disposition and send it.
     });
   });
