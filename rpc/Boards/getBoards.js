4c9bdde4dc2b4803886c603e5ef5032e622a972c
陳泓均 <hankchen@ekopro.solutions>
2018-01-08 13:29:47 +0800

    fix(Boards/getBoards):remove messageData while getBoards to reduce network resource

--- rpc/Boards/getBoards.js
+++ rpc/Boards/getBoards.js
@@ -7,7 +7,14 @@ export default function getBoards(socket, db) {
   socket.on('getBoards', data => {
     const boardDb = db.collection('Boards');
     boardDb.find().toArray((err, result) => {
-      socket.emit('getBoardsResponse', new Promise(resolve => resolve(success(result))));
+      socket.emit(
+        'getBoardsResponse',
+        new Promise(resolve => {
+          const boards = result;
+          boards.map(x => delete x.messageData);
+          resolve(success(result));
+        })
+      );
     });
   });
 }
